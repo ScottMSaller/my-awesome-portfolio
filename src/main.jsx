5440250b9ec74780e5101c27c027b4efd80b01c6
Scott Saller <Scottmsaller@gmail.com>
2024-09-21 15:20:06 -0400

    Added react-dom-router to the project and specified both paths and elements for the contents of the web page. Also added an error-page.jsx, which will be used in the case of a 404.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
+import * as React from "react";
+// import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+//main.jsx is the container for our router and it's sole purpose is that and that alone
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/portfolio",
+    element: <Portfolio/>
+  },
+  {
+    path: "/contact",
+    element: <Contact/>
+  },
+  {
+    path: "/about-me",
+    element: <AboutMe/>
+  },
+  {
+    path: "/resume",
+    element: <Resume/>
+  }
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
